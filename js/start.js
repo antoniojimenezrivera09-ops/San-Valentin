3923b524747107dc0a66fb2fb56b354c692ef292
antoniojimenezrivera09-ops <antoniojimenezrivera09@gmail.com>
2026-02-10 07:43:14 -0500

    Delete js/start.js

--- js/start.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("keydown", () => {
-  window.location.href = "msg.html" + window.location.search;
-});
-
-document.addEventListener("click", () => {
-  window.location.href = "msg.html" + window.location.search;
-});
