33b4c72de6b240df247872a263bd7e83964a9b1a
antoniojimenezrivera09-ops <antoniojimenezrivera09@gmail.com>
2026-02-10 07:26:06 -0500

    Create start.js

---
+++ js/start.js
@@ -0,0 +1,7 @@
+document.addEventListener("keydown", () => {
+  window.location.href = "msg.html" + window.location.search;
+});
+
+document.addEventListener("click", () => {
+  window.location.href = "msg.html" + window.location.search;
+});
